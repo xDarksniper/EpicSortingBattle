0883235ca7ff413f45e084c1690b0ce27321a7bd
Rotten <thorbjoern.j@gmail.com>
2022-08-18 10:58:40 +0200

    ny sej funktiomn

--- sorteringsAlgoritmer.py
+++ sorteringsAlgoritmer.py
@@ -42,8 +42,8 @@ if __name__ == '__main__':
 
 def bubblesort(array):
     for i in range(len(array)):
-        for j in range ((array)-1):
-            if array[j]>array[j+1]:
+        for j in range(len(array)-1):
+            if array[j] > array[j+1]:
                 array[j], array[j+1] = array[j+1], array[j]
 
 array = [2,5,8,0,1,5,4]
