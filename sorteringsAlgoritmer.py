b81d22d4bc2ee057fea6eadcf764c3f288cd39f7
Rotten <thorbjoern.j@gmail.com>
2022-08-19 12:23:47 +0200

    ny sej funktiomn

--- sorteringsAlgoritmer.py
+++ sorteringsAlgoritmer.py
@@ -1,42 +1,8 @@
 import random, copy
 
-def bogoSort(items):
-    # Kopier den liste, vi har modtaget som parameter, så vi ikke ændrer den originale
-    items = items.copy()
-    isSorted = None # Boolean til markering af, om listen er sorteret
-    attempts = 0 # Tællevariabel til at holde styr på antal af forsøg
-    while not isSorted:
-        attempts += 1
-        if attempts > len(items) * 5000: # Check for at stoppe tendensen mod uendeligt
-            print('Giver op på grund af for mange forsøg ({}) og bruger TimSort'.format(attempts))
-            items.sort()
-            return items
-        random.shuffle(items) # Bland alle elementer helt tilfældigt
-        isSorted = True # Vi går ud fra at listen tilfældigvis er sorteret,
-        # ...og prøver i denne løkke at bevise det modsatte
-        for index in range(len(items)-1):
-            if items[index] > items[index+1]:
-                isSorted = False
-                break # Bryd løkken hvis et eneste element er forkert sorteret
-    print('Sorteret efter {} forsøg'.format(attempts))
-    return items
 
 
-if __name__ == '__main__':
-    l = list(range(0, 5))
-    lb = l.copy()
-    for i in range(50):
-        random.shuffle(lb)
-        ## Kald den funktion, du vil teste
-        ls = bogoSort(l)
-        ## Kald den funktion, du vil teste
-        if ls != l:
-            print('Fejl! Algoritmen kan ikke sortere.')
-            break
-    print('Succes! Algoritmen sorterer korrekt.')
-    print('blandet: ', lb)
-    print('sorteret:', ls)
-
+"""""
 def abubbleSort(array):
     for o in enumerate(array):
         for i in enumerate(array):
@@ -44,20 +10,21 @@ def abubbleSort(array):
                     temp = array[i+1]
                     array[i+1] = array[i]
                     array[i] = temp
+"""
 
-
-def bubblesort(array):
+def bubbleSort(array):
     for i in range(len(array)):
         for j in range(len(array)-1):
             if array[j] > array[j+1]:
                 array[j], array[j+1] = array[j+1], array[j]
+    return array
 
-array = [2,5,8,0,1,5,4]
-bubblesort(array)
-print("bubblesort")
-print(array)
 
-def insertionsort(array):
+
+
+
+
+def insertionSort(array):
     for i in range(1, len(array)):
         key = array[i]
         l = i-1
@@ -65,15 +32,19 @@ def insertionsort(array):
             array[l+1] = array[l]
             l = l-1
         array[l+1]= key
+    return array
 
-array = [2,5,8,0,1,5,4]
-insertionsort(array)
-print("insertionsort")
-print(array)
 
-def insertionSort(arr):
-    for i in range(len(arr)):
+
+
+def insertionSort(array):
+    for i in range(len(array)):
         while i>i+1:
             array[i], array[i+1] = array[i+1], array[i]
-        while i>i-1:
+        if i>i-1:
             array[i], array[i-1] = array[i-1], array[i]
+            return array
+
+
+
+
