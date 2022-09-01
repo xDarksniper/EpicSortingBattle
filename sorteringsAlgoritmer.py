688248589fe4ab0bc22466585edfe608aca4b15c
Fusker <nfr@nyraad.net>
2022-09-01 09:49:12 +0200

    Fixet bubblesort

--- sorteringsAlgoritmer.py
+++ sorteringsAlgoritmer.py
@@ -28,7 +28,7 @@ def abubbleSort(array):
 
 def bubbleSort(array):
     for i in range(len(array)):
-        for j in range(len(array)-1):
+        for j in range(len(array)-1-i):
             if array[j] > array[j+1]:
                 array[j], array[j+1] = array[j+1], array[j]
     return array
